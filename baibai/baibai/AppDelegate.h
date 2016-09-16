4c47564ec6d328d4174e60d41741bbc292c07e72
baisi <328550191@qq.com>
2016-09-17 01:23:02 +0800

    新建项目

---
+++ baibai/baibai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  baibai
+//
+//  Created by litoyo on 16/9/17.
+//  Copyright © 2016年 litoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ baibai/baibai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baibai
+//
+//  Created by litoyo on 16/9/17.
+//  Copyright © 2016年 litoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
